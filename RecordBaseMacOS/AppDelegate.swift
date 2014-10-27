bda76874e3a26c35db30a7c859d3a74b0889148f
Ben Parham <benparham391@gmail.com>
2014-10-27 12:59:50 -0400

    Initial Commit

---
+++ RecordBaseMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RecordBaseMacOS
+//
+//  Created by Ruben Parham on 10/27/14.
+//  Copyright (c) 2014 Ruben Parham. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RecordBaseMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RecordBaseMacOS
+//
+//  Created by Ruben Parham on 10/27/14.
+//  Copyright (c) 2014 Ruben Parham. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
